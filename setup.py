7c58834e640ed435198b538732034307d043e86a
Suhas Kulkarni <suhaskulkarni5492@gmail.com>
2025-03-25 14:13:03 +0530

    installed requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Info Retrieval project',
+    version= '0.0.0',
+    author= 'Suhas',
+    packages= find_packages(),
+    install_requires = []   
+
+)
